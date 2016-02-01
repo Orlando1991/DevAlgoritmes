f1ff17175c20b441ce62fc190e00f29108e1db70
Orlando1991 <osd_1991@hotmail.com>
2016-02-01 02:26:44 +0100

    Assignment 1+2
    Assignment 1+2 done

---
+++ EntryPoint/KDTree.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+namespace EntryPoint
+
+{
+    public class Node
+    {
+        public Vector2 root { get; set; }
+        public Node left { get; set; }
+        public Node right { get; set; }
+    }
+
+    class KDTree
+    {
+        public List<Vector2> PotentialBuildingsInRange;
+
+        public KDTree()
+        {
+            PotentialBuildingsInRange = new List<Vector2>();
+        }
+
+
+        public Node MakeTree(List<Vector2> points, int depth)
+        {
+
+            if (points.Count == 0)
+            {
+                return null;
+            }
+
+            Vector2 middle;
+            var left = new List<Vector2>();
+            var right = new List<Vector2>();
+
+            if (depth % 2 == 0)
+            {
+                middle = SortByCoordinate(points, p => p.X, left, right);
+            }
+            else
+            {
+                middle = SortByCoordinate(points, p => p.Y, left, right);
+            }
+
+
+            return new Node
+            {
+                root = middle,
+                left = MakeTree(left, depth + 1),
+                right = MakeTree(right, depth + 1)
+            };
+        }
+
+        private static Vector2 SortByCoordinate(List<Vector2> points, Func<Vector2, float> coordinate, List<Vector2> left, List<Vector2> right)
+        {
+            points = points.OrderBy(coordinate).ToList();
+            var middle = points[points.Count / 2];
+            points.Remove(middle);
+            foreach (var item in points)
+            {
+                if (coordinate(item) <= coordinate(middle))
+                {
+                    left.Add(item);
+                }
+                else if (coordinate(item) > coordinate(middle))
+                {
+                    right.Add(item);
+                }
+            }
+            return middle;
+        }
+
+        public void RangeSearch(Vector2 house, Node rootNode, float maxDistance, int depth, Func<Vector2, float>[] coordinates)
+        {
+            if (rootNode == null)
+            {
+                return;
+            }
+            var rootVector = coordinates[depth % 2](rootNode.root);
+            var houseVector = coordinates[depth % 2](house);
+
+            if (rootVector < (houseVector - maxDistance))
+            {
+                RangeSearch(house, rootNode.right, maxDistance, depth + 1, coordinates);
+            }
+            else if (rootVector > (houseVector + maxDistance))
+            {
+                RangeSearch(house, rootNode.left, maxDistance, depth + 1, coordinates);
+            }
+            else if ((rootVector >= (houseVector - maxDistance)) && (rootVector <= (houseVector + maxDistance)))
+            {
+                PotentialBuildingsInRange.Add(rootNode.root);
+                RangeSearch(house, rootNode.left, maxDistance, depth + 1, coordinates);
+                RangeSearch(house, rootNode.right, maxDistance, depth + 1, coordinates);
+            }
+        }
+    }
+}
---
+++ EntryPoint/Merge.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace EntryPoint
+{
+    class Merge
+    {
+        public List<Tuple<Vector2, float>> mergesort(List<Tuple<Vector2, float>> a)
+        {
+            // If list size = 0 or 1 list is sorted
+            if (a.Count <= 1) return a;
+
+
+            int midpoint = a.Count / 2;
+            List<Tuple<Vector2, float>> Left = new List<Tuple<Vector2, float>>();
+            List<Tuple<Vector2, float>> Right = new List<Tuple<Vector2, float>>();
+
+            //split list in 2
+            for (int i = 0; i < midpoint; i++)
+                Left.Add(a[i]);
+            for (int i = midpoint; i < a.Count; i++)
+                Right.Add(a[i]);
+
+            // MergeSort() to split listss
+            Left = mergesort(Left);
+            Right = mergesort(Right);
+
+            // Merge the split lists
+            return merge(Left, Right);
+        }
+
+        public static List<Tuple<Vector2, float>> merge(List<Tuple<Vector2, float>> Left, List<Tuple<Vector2, float>> Right)
+        {
+            // keep looping untill 1 list remains
+            List<Tuple<Vector2, float>> result = new List<Tuple<Vector2, float>>();
+            while (Left.Count > 0 && Right.Count > 0)
+            {
+                if (Left[0].Item2 < Right[0].Item2)
+                {
+                    result.Add(Left[0]);
+                    Left.RemoveAt(0);
+                }
+                else
+                {
+                    result.Add(Right[0]);
+                    Right.RemoveAt(0);
+                }
+
+            }
+            while (Left.Count > 0)
+            {
+                result.Add(Left[0]);
+                Left.RemoveAt(0);
+            }
+            while (Right.Count > 0)
+            {
+                result.Add(Right[0]);
+                Right.RemoveAt(0);
+            }
+            return result;
+        }
+
+
+    }
+}
